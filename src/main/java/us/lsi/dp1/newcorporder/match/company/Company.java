14c78b1d81934d149cfeb8a24e904f6722fe49bb
Marco Anarmo <marcoanarmo@gmail.com>
2023-10-17 20:09:50 +0200

    feat: create company stuff

---
+++ src/main/java/us/lsi/dp1/newcorporder/match/company/Company.java
@@ -0,0 +1,38 @@
+package us.lsi.dp1.newcorporder.match.company;
+
+import lombok.Getter;
+
+@Getter
+@SuppressWarnings("SpellCheckingInspection")
+public enum Company {
+
+    SLIMGROTZ_INC(CompanyType.BROADCAST_NETWORK),
+    FLICKERING_LIGHTS(CompanyType.BROADCAST_NETWORK),
+    GLOBAL_CORP(CompanyType.BROADCAST_NETWORK),
+
+    PAGE_ONE_CORP(CompanyType.PRINT_MEDIA),
+    READALOT(CompanyType.PRINT_MEDIA),
+    GENERTIC_SUB_INC(CompanyType.PRINT_MEDIA),
+
+    SUMLIMINAL_SUBSIDIARY(CompanyType.GUERRILLA_MARKETING),
+    VISUAL_TERROR_INC(CompanyType.GUERRILLA_MARKETING),
+    WORDOFMOUTH(CompanyType.GUERRILLA_MARKETING),
+
+    WALLPAPER(CompanyType.AMBIENT_ADVERTISING),
+    HOLOGRAFX(CompanyType.AMBIENT_ADVERTISING),
+    ALL_AROUND_YOU(CompanyType.AMBIENT_ADVERTISING),
+
+    PIX_CHIX(CompanyType.ONLINE_MARKETING),
+    PRETTY_OBSCURE_INC(CompanyType.ONLINE_MARKETING),
+    CLICKBAITER(CompanyType.ONLINE_MARKETING),
+
+    ANONYMOUS_CROWD(CompanyType.SOCIAL_MEDIA),
+    LEADERBOARDER(CompanyType.SOCIAL_MEDIA),
+    XCURBR(CompanyType.SOCIAL_MEDIA);
+
+    private final CompanyType type;
+
+    Company(CompanyType type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/us/lsi/dp1/newcorporder/match/company/CompanyType.java
@@ -0,0 +1,5 @@
+package us.lsi.dp1.newcorporder.match.company;
+
+public enum CompanyType {
+    BROADCAST_NETWORK, PRINT_MEDIA, GUERRILLA_MARKETING, AMBIENT_ADVERTISING, ONLINE_MARKETING, SOCIAL_MEDIA
+}

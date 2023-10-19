44ae409c1d16229b6aec8c4f67ffb7bd21b39233
Marco Anarmo <marcoanarmo@gmail.com>
2023-10-19 19:18:37 +0200

    feat: add agents logic to CompanyTile

--- src/main/java/us/lsi/dp1/newcorporder/match/company/CompanyTile.java
+++ src/main/java/us/lsi/dp1/newcorporder/match/company/CompanyTile.java
@@ -1,17 +1,39 @@
 package us.lsi.dp1.newcorporder.match.company;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
+import com.google.common.base.Preconditions;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import us.lsi.dp1.newcorporder.match.conglomerate.Conglomerate;
 
 @Getter
+@EqualsAndHashCode(of = "company")
 public class CompanyTile {
 
     private final Company company;
-    private final Multiset<Conglomerate> agents = HashMultiset.create();
 
-    public CompanyTile(Company company) {
+    private Conglomerate currentConglomerate;
+    private int agents = 1;
+
+    public CompanyTile(Company company, Conglomerate currentConglomerate) {
         this.company = company;
+        this.currentConglomerate = currentConglomerate;
+    }
+
+    public void addAgents(int num) {
+        Preconditions.checkArgument(num > 0, "num cannot be zero");
+        this.agents += num;
+    }
+
+    public void removeAgent(int num) {
+        Preconditions.checkArgument(num > 0, "num cannot be zero");
+        Preconditions.checkState(this.agents > 1, "cannot leave a tile empty");
+        this.agents -= num;
+    }
+
+    public void attack(Conglomerate conglomerate, int agents) {
+        Preconditions.checkArgument(conglomerate != this.currentConglomerate, "cannot attack with the current conglomerate");
+        Preconditions.checkArgument(agents > this.agents, "unsuccessful attack");
+        this.currentConglomerate = conglomerate;
+        this.agents = agents;
     }
 }

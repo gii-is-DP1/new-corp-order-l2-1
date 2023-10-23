9dfadf50321d85033f5d3a22b34dd20cdf933709
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2023-10-23 18:03:05 +0200

    feat: finish Match module skeleton

---
+++ src/main/java/us/lsi/dp1/newcorporder/match/Match.java
@@ -0,0 +1,48 @@
+package us.lsi.dp1.newcorporder.match;
+
+import lombok.Getter;
+import us.lsi.dp1.newcorporder.match.company.CompanyMatrix;
+import us.lsi.dp1.newcorporder.match.player.MatchPlayer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class Match {
+
+    /**
+     * Creates a new match for the given configuration
+     *
+     * @param maxPlayers the max number of players that the match would hold
+     * @param matchMode  the mode of the match
+     * @return the new match
+     */
+    public static Match create(int maxPlayers, MatchMode matchMode) {
+        GeneralSupply generalSupply = GeneralSupply.create();
+        CompanyMatrix companyMatrix = CompanyMatrix.create();
+        String inviteCode = "foo"; // TODO make code random
+
+        return new Match(maxPlayers, matchMode, inviteCode, generalSupply, companyMatrix);
+    }
+
+    private final int maxPlayers;
+    private final MatchMode matchMode;
+    private final String inviteCode;
+
+    private final Map<Integer, MatchPlayer> players = new HashMap<>();
+
+    private final GeneralSupply generalSupply;
+    private final CompanyMatrix companyMatrix;
+
+    private Match(int maxPlayers, MatchMode matchMode, String inviteCode, GeneralSupply generalSupply, CompanyMatrix companyMatrix) {
+        this.maxPlayers = maxPlayers;
+        this.matchMode = matchMode;
+        this.inviteCode = inviteCode;
+        this.generalSupply = generalSupply;
+        this.companyMatrix = companyMatrix;
+    }
+
+    public MatchPlayer getMatchPlayer(int playerId) {
+        return this.players.get(playerId);
+    }
+}

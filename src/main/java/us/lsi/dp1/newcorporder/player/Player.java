d7bb8f139037be7e8477c0039edccd539597e7e2
VictoriaCRD <vicruidel1@alum.us.es>
2023-11-06 13:06:44 +0100

    fix: updated Player with review changes

--- src/main/java/us/lsi/dp1/newcorporder/player/Player.java
+++ src/main/java/us/lsi/dp1/newcorporder/player/Player.java
@@ -1,40 +1,22 @@
 package us.lsi.dp1.newcorporder.player;
+
 import jakarta.persistence.*;
-import us.lsi.dp1.newcorporder.user.User;
-import jakarta.validation.constraints.NotNull;
-import org.apache.commons.lang3.ArrayUtils;
-import us.lsi.dp1.newcorporder.authority.Authority;
-import us.lsi.dp1.newcorporder.model.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
+import us.lsi.dp1.newcorporder.model.BaseEntity;
+import us.lsi.dp1.newcorporder.user.User;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "players")
+
 public class Player extends BaseEntity {
+
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user", referencedColumnName = "username")
-    private User user;
+    @JoinColumn(name = "user", referencedColumnName = "username") private User user;
 
-
-
-    public String toString(){
-        return user.getUsername();
-    }
-
-    public static Player createDefault() {
-
-        Player player = new Player();
-
-
-        player.setId(0);
-
-        player.setUser(null);
-
-        return player;
-    }
-
-
+    public String toString() {return user.getUsername();}
 
 
 }

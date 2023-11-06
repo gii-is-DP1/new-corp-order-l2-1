1c5eddb4ce1a6b55a59c92caeac0dfdf363b4c9f
VictoriaCRD <vicruidel1@alum.us.es>
2023-11-06 13:10:49 +0100

    fix: updated PlayerRepository with review changes

--- src/main/java/us/lsi/dp1/newcorporder/player/PlayerRepository.java
+++ src/main/java/us/lsi/dp1/newcorporder/player/PlayerRepository.java
@@ -2,18 +2,11 @@ package us.lsi.dp1.newcorporder.player;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import us.lsi.dp1.newcorporder.user.*;
 
-public interface PlayerRepository extends CrudRepository<Player, Integer>{
-
-    @Query("SELECT p FROM Player p")
+public interface PlayerRepository extends CrudRepository<Player, Integer> {
     Page<Player> findAllPlayers(Pageable pageable);
 
-
-
-    @Query("SELECT p FROM Player p WHERE p.user.username LIKE :username")
     Player findPlayerByUsername(@Param("username") String username);
 }

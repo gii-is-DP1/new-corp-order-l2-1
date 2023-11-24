ef7e61fe7bb8371e254bc5857f4a23c318751a7a
VictoriaCRD <vicruidel1@alum.us.es>
2023-11-24 11:55:02 +0100

    feat: created PlayerMatchStatsService

---
+++ src/main/java/us/lsi/dp1/newcorporder/stats/PlayerMatchStatsService.java
@@ -0,0 +1,29 @@
+package us.lsi.dp1.newcorporder.stats;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class PlayerMatchStatsService {
+
+    private final MatchStatsService matchStatsService;
+
+    @Autowired
+    public PlayerMatchStatsService(MatchStatsService matchStatsService) {
+        this.matchStatsService = matchStatsService;
+    }
+
+    public Optional<MatchStats> getMatchStatsById(Long matchId) {
+        return matchStatsService.getMatchStatsById(matchId);
+    }
+
+    public Set<PlayerMatchStats> getPlayerMatchStats(Long matchId) {
+        Optional<MatchStats> matchStatsOptional = matchStatsService.getMatchStatsById(matchId);
+        return matchStatsOptional.map(MatchStats::getPlayerMatchStats).orElse(Collections.emptySet());
+    }
+
+}

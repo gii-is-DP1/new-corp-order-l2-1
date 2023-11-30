eb97d495705452cfd1c3124ebab9646b5ae695ad
Marco Anarmo <marcoanarmo@gmail.com>
2023-11-30 18:13:56 +0100

    feat: create main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,7 @@ import {ErrorBoundary} from "react-error-boundary";
 import tokenService from "./services/token.service";
 import SwaggerDocs from "./public/swagger";
 import Match from "./match/Match"
-import AppNavbar from "./AppNavbar";
-
-//import {Match} from "msw";
+import {MainPage} from "./MainPage";
 
 function ErrorFallback({error, resetErrorBoundary}) {
     return (
@@ -36,10 +34,10 @@ function App() {
         <div>
             <ErrorBoundary FallbackComponent={ErrorFallback}>
                 <Routes>
+                    <Route path="/" element={<MainPage/>}/>
                     <Route path="/match/:id" element={<Match/>}/>
                     <Route path="/docs" element={<SwaggerDocs/>}/>
                 </Routes>
-                <AppNavbar/>
             </ErrorBoundary>
         </div>
     );

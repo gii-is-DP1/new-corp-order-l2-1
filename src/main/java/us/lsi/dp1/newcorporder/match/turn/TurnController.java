11f35aeb9ecaad03d6a4a5159be081d848c70dfa
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2023-12-07 19:42:39 +0100

    fix: switch to FromPathVariable annotations

--- src/main/java/us/lsi/dp1/newcorporder/match/turn/TurnController.java
+++ src/main/java/us/lsi/dp1/newcorporder/match/turn/TurnController.java
@@ -3,6 +3,7 @@ package us.lsi.dp1.newcorporder.match.turn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+import us.lsi.dp1.newcorporder.bind.FromPathVariable;
 import us.lsi.dp1.newcorporder.match.Match;
 import us.lsi.dp1.newcorporder.match.VerifyCurrentTurn;
 import us.lsi.dp1.newcorporder.match.payload.request.*;
@@ -15,49 +16,49 @@ public class TurnController {
 
     @PostMapping
     @VerifyCurrentTurn
-    public void selectTurn(@PathVariable Match match, @RequestParam("action") Action action) {
+    public void selectTurn(@RequestParam("action") Action action, @FromPathVariable Match match) {
         match.getTurnSystem().selectAction(action);
     }
 
     @PostMapping("/plot")
     @VerifyCurrentTurn
-    public PlotResponse plot(@PathVariable Match match, @RequestBody @Valid PlotRequest request) {
+    public PlotResponse plot(@RequestBody @Valid PlotRequest request, @FromPathVariable Match match) {
         return match.getTurnSystem().getCurrentTurn().onPlotRequest(request);
     }
 
     @DeleteMapping("/shares")
     @VerifyCurrentTurn
-    public DiscardShareResponse discardShare(@PathVariable Match match, @RequestBody @Valid DiscardShareRequest request) {
+    public DiscardShareResponse discardShare(@RequestBody @Valid DiscardShareRequest request, @FromPathVariable Match match) {
         return match.getTurnSystem().getCurrentTurn().onDiscardShareRequest(request);
     }
 
     @PostMapping("/consultants/use")
     @VerifyCurrentTurn
-    public UseConsultantResponse useConsultant(@PathVariable Match match, @RequestBody @Valid UseConsultantRequest request) {
+    public UseConsultantResponse useConsultant(@RequestBody @Valid UseConsultantRequest request, @FromPathVariable Match match) {
         return match.getTurnSystem().getCurrentTurn().onUseConsultantRequest(request);
     }
 
     @PostMapping("/infiltrate")
     @VerifyCurrentTurn
-    public InfiltrateResponse infiltrate(@PathVariable Match match, @RequestBody @Valid InfiltrateRequest request) {
+    public InfiltrateResponse infiltrate(@RequestBody @Valid InfiltrateRequest request, @FromPathVariable Match match) {
         return match.getTurnSystem().getCurrentTurn().onInfiltrateRequest(request);
     }
 
     @PostMapping("/consultants/take")
     @VerifyCurrentTurn
-    public void takeConsultant(@PathVariable Match match, @RequestBody @Valid TakeConsultantRequest request) {
+    public void takeConsultant(@RequestBody @Valid TakeConsultantRequest request, @FromPathVariable Match match) {
         match.getTurnSystem().getCurrentTurn().onTakeConsultantRequest(request);
     }
 
     @PostMapping("/take-over")
     @VerifyCurrentTurn
-    public TakeOverResponse takeOver(@PathVariable Match match, @RequestBody @Valid TakeOverRequest request) {
+    public TakeOverResponse takeOver(@RequestBody @Valid TakeOverRequest request, @FromPathVariable Match match) {
         return match.getTurnSystem().getCurrentTurn().onTakeOverRequest(request);
     }
 
     @PostMapping("/company-abilities")
     @VerifyCurrentTurn
-    public UseCompanyAbilityResponse useCompanyAbility(@PathVariable Match match, @RequestBody @Valid UseCompanyAbilityRequest request) {
+    public UseCompanyAbilityResponse useCompanyAbility(@RequestBody @Valid UseCompanyAbilityRequest request, @FromPathVariable Match match) {
         return match.getTurnSystem().getCurrentTurn().onUseCompanyAbilityRequest(request);
     }
 }

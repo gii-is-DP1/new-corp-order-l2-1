767c8f851b107870d805a7ed8fc1d774d2f0dd86
Marco Anarmo <marcoanarmo@gmail.com>
2023-12-08 12:21:29 +0100

    feat: add OpenAPI documentation to AchievementController

--- src/main/java/us/lsi/dp1/newcorporder/achievement/AchievementController.java
+++ src/main/java/us/lsi/dp1/newcorporder/achievement/AchievementController.java
@@ -1,5 +1,7 @@
 package us.lsi.dp1.newcorporder.achievement;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +24,57 @@ public class AchievementController {
         this.achievementService = achievementService;
     }
 
+    @Operation(
+        summary = "List all achievements",
+        description = "List all achievements",
+        tags = "get"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "The achievements list"
+    )
     @GetMapping
     public ResponseEntity<List<Achievement>> findAll() {
         List<Achievement> res = (List<Achievement>) achievementService.findAll();
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "Find an achievement by its id",
+        description = "Find an achievement by its id",
+        tags = "get"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "The found achievement"
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Achievement not found"
+    )
     @GetMapping(value = "{id}")
     public ResponseEntity<Achievement> findById(@PathVariable("id") Integer id) {
         Achievement res = achievementService.findById(id);
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "Create an achievement",
+        description = "Create an achievement",
+        tags = "post"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "The created achievement"
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Not valid achievement"
+    )
+    @ApiResponse(
+        responseCode = "401",
+        description = "Authorization information is missing or invalid"
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Achievement> create(@RequestBody @Valid Achievement achievement) {
@@ -41,6 +82,23 @@ public class AchievementController {
         return new ResponseEntity<>(savedAchievement, HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "Update an achievement by its id",
+        description = "Update an achievement by its id",
+        tags = "put"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "The updated achievement"
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Not valid achievement"
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Achievement to update not found"
+    )
     @PutMapping(value = "{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Achievement> update(@PathVariable("id") Integer id, @RequestBody @Valid Achievement achievement) {
@@ -48,6 +106,19 @@ public class AchievementController {
         return new ResponseEntity<>(this.achievementService.updateAchievement(achievement, id), HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "Delete an achievement by its id",
+        description = "Delete an achievement by its id",
+        tags = "delete"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Achievement deleted"
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "Achievement to delete not found"
+    )
     @DeleteMapping(value = "{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<MessageResponse> delete(@PathVariable("id") int id) {

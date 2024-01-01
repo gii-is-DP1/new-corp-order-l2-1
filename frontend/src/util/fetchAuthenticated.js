6381b9d30c0dd6cbee12472fdd7ef7849b967027
Marco Anarmo <marcoanarmo@gmail.com>
2024-01-01 19:09:43 +0100

    fix: handle errors in fetchAuthenticated

--- frontend/src/util/fetchAuthenticated.js
+++ frontend/src/util/fetchAuthenticated.js
@@ -1,11 +1,16 @@
 import tokenService from "../services/token.service";
 
 export default async function fetchAuthenticated(url, method) {
-    return await fetch(url, {
+    const response = await fetch(url, {
         method: method,
         headers: {
             "Authorization": `Bearer ${tokenService.getLocalAccessToken()}`,
             "Content-Type": "application/json"
         },
     })
+    if (!response.ok) {
+        throw new Error(await response.json().then(error => error.message))
+    } else {
+        return response;
+    }
 }

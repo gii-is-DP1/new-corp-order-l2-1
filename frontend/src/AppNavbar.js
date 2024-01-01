4efe874c33c084139d1cae5552aec12598105de6
Marco Anarmo <marcoanarmo@gmail.com>
2024-01-01 18:17:08 +0100

    fix: update navbar with current routes

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import tokenService from './services/token.service';
 import jwt_decode from 'jwt-decode';
 import {black, white} from "./util/Colors";
 import {Text} from "./components/Text";
+import Button, {ButtonType} from "./components/Button";
 
 const AppNavbar = () => {
     const [roles, setRoles] = useState([]);
     const [username, setUsername] = useState('');
     const jwt = tokenService.getLocalAccessToken();
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (jwt) {
@@ -22,30 +24,29 @@ const AppNavbar = () => {
 
     if (!jwt) {
         navLinks = createNavLinks([
-            {link: '/home', text: 'HOME'},
-            {link: '/register', text: 'REGISTER'},
-            {link: '/login', text: 'LOGIN'},
+            {link: '/register', text: 'Register'},
+            {link: '/login', text: 'Login'},
         ]);
     } else {
         roles.forEach((role) => {
             if (role === 'ADMIN') {
                 navLinks = createNavLinks([
-                    {link: '/matches', text: 'MATCHES'},
-                    {link: '/moderation', text: 'MODERATION'},
-                    {link: '/achievements', text: 'ACHIEVEMENTS'},
-                    {link: '/stats', text: 'STATS'},
-                    {link: '/boh', text: 'LEAVE ADMIN PANEL'},
+                    {link: '/matches', text: 'Matches'},
+                    {link: '/moderation', text: 'Moderation'},
+                    {link: '/achievements', text: 'Achievements'},
+                    {link: '/stats', text: 'Stats'},
+                    {link: '/boh', text: 'Leave Admin Panel'},
                 ]);
             }
             if (role === 'USER') {
                 navLinks = createNavLinks([
-                    {link: '/stats', text: 'STATS'},
-                    {link: '/friends', text: 'FRIENDS'},
-                    {link: '/boh', text: 'PLAY NOW'},
+                    {link: `/user/${username}/friends`, text: 'Friends'},
+                    {link: `/user/${username}/stats`, text: 'Stats'},
+                    {link: `/user/${username}`, text: 'Profile'},
+                    {link: '', text: 'Play now', isButton: true},
                 ]);
             }
         });
-
     }
 
     const navBarStyle = {

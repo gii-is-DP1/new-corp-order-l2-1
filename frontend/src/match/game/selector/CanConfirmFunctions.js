e2a9212fe169e4fc693ce3c980843d187cf9e1b6
Michelangelo1709 <michelangelo.bertoli00@outlook.it>
2024-01-14 12:03:07 +0100

    feat: BroadcastNetwork backend

--- frontend/src/match/game/selector/CanConfirmFunctions.js
+++ frontend/src/match/game/selector/CanConfirmFunctions.js
@@ -10,6 +10,10 @@ export function selectAtLeastTwo(selection, selectedElements){
     return selectedElements.length >= 2;
 }
 
+export function selectAtLeastTwoOrThree(selection, selectedElements){
+    return selectedElements.length >= 2 && selectedElements.length <= 3;
+}
+
 export function selectAtLeastOneOrTwo(selection, selectedElements){
     return selectedElements.length >= 1 && selectedElements.length <= 2;
 }
--- frontend/src/match/game/selector/pickers/Pickers.js
+++ frontend/src/match/game/selector/pickers/Pickers.js
@@ -3,7 +3,7 @@ import {
     selectAtLeastOne,
     selectAtLeastOneOrTwo,
     selectAtLeastOneOrZero,
-    selectAtLeastTwo
+    selectAtLeastTwo, selectAtLeastTwoOrThree
 } from "../CanConfirmFunctions";
 import {
     onlySelectOfSameColor,
@@ -93,6 +93,17 @@ export function pickOneOrTwoCompanies(from, onConfirm) {
         selection={from}
         canConfirm={selectAtLeastOneOrTwo}
         changeSelectableItems={onlySelectOfSameColor}
+        onConfirm={onConfirm}
+        key={{onConfirm}}
+    />;
+}
+
+export function pickTwoOrThreeCompanies(from, onConfirm) {
+    return <CompanySelector
+        title={"Pick two or three companies"}
+        selection={from}
+        canConfirm={selectAtLeastTwoOrThree}
+        changeSelectableItems={selectQuantity(3)}
         onConfirm={onConfirm}
         key={{onConfirm}}
     />;

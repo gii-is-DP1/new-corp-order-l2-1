51a89ef800c0619d50f197d9166eea56e4bc0bae
Michelangelo1709 <michelangelo.bertoli00@outlook.it>
2024-01-14 19:41:12 +0100

    feat: OnlineMarketing ability backend

--- frontend/src/match/game/views/takeover/OnlineMarketingCompaniesState.js
+++ frontend/src/match/game/views/takeover/OnlineMarketingCompaniesState.js
@@ -2,18 +2,33 @@ import {FrontendState} from "../FrontendState";
 import {useContext} from "react";
 import {StateContext} from "../../Game";
 import {pickTwoCompanies} from "../../selector/pickers/Pickers";
+import fetchAuthenticatedWithBody from "../../../../util/fetchAuthenticatedWithBody";
+import {Info} from "../../../Match";
 
 function OnlineMarketingCompaniesPicker(){
-    const context= useContext(StateContext);
+    const context = useContext(StateContext);
+    const info = useContext(Info);
     const state = context.state;
     return pickTwoCompanies(context.companyTiles.components, (selected) => {
         state.takeover.ability.onlineMarketing.companies = selected.map(index => state.game.companies[index]);
-        const firstCompanyType = state.game.companies[selected[0]].type;
+        const body = {companyAbility: {type: "OnlineMarketingAbility",
+                firstCompany: {x: selected[0] % 4, y: Math.floor(selected[0] / 4)},
+                secondCompany: {x: selected[1] % 4, y: Math.floor(selected[1] / 4)}}};
+        const fetchCompany = async () => {
+            try {
+                await fetchAuthenticatedWithBody(`/api/v1/matches/${info.code}/turn/company-abilities`, "POST", body)
+                    .then(async response => await response.json());
+            } catch (error) {
+                console.log(error.message)
+            }
+        };
+        fetchCompany();
+        /*const firstCompanyType = state.game.companies[selected[0]].type;
         state.game.companies[selected[0]].type = state.game.companies[selected[1]].type;
         state.game.companies[selected[1]].type = firstCompanyType;
         const firstCompanyAgents = state.game.companies[selected[0]].agents;
         state.game.companies[selected[0]].agents = state.game.companies[selected[1]].agents;
-        state.game.companies[selected[1]].agents = firstCompanyAgents;
+        state.game.companies[selected[1]].agents = firstCompanyAgents;*/
         context.update();
     })
 }
